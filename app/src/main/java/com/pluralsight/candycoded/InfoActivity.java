c8f959d714a83b85ef7b0830b3b2d54d5c37a8b9
mariawavua <wavuamwakidedi@gmail.com>
2020-10-01 20:42:16 +0300

    Second commit

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,8 +1,10 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -15,13 +17,25 @@ public class InfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info);
 
         Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
-        ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
+        ImageView candyStoreImageView = (ImageView) findViewById(R.id.image_view_candy_store);
         Picasso.with(this).
                 load(uri).
+
                 into(candyStoreImageView);
-
-
     }
+
+    public void createMapIntent(View view) {
+        Uri uriAddress = Uri.parse("geo:0, 0?q=618 E South St Orlando, Fl 32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriAddress);
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+    }
+
+
+}
 
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
@@ -30,4 +44,4 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
-}
+
